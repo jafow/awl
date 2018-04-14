26222dd4e3b9283d5bfe26b3a20c9f53be17e2c3
jafow <jared.a.fowler@gmail.com>
2018-04-14 14:01:04 -0700

    Add mock lib and import into main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate awl;
+
+use awl::Awl;
+
 fn main() {
     println!("awl is a tool to punch holes with.");
+    let a = Awl {
+        ip_addr: "44.00.32.14".to_string(),
+        port: 1337
+    };
+
+    a.my_ip();
 }
