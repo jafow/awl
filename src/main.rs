6ca54b81af691f831658e19635800070e5f7cbae
jafow <jared.a.fowler@gmail.com>
2018-04-16 14:15:36 -0700

    WIP server cli api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+
+extern crate awl;
+use std::env;
+use awl::Awl;
+
 fn main() {
     println!("awl is a tool to punch holes with.");
+
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+
+    // let a = Awl {
+    //     ip_addr: "44.00.32.14".to_string(),
+    //     port: 1337
+    // };
+
+    // a.my_ip();
 }
