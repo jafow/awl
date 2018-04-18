a6eb2fa966b954bda30523024663649943b9e401
Allan Calix <acalix0592@gmail.com>
2018-04-17 23:50:52 -0700

    Refactor to focus on IPv4 support

--- lib/connection.rs
+++ lib/connection.rs
@@ -1,21 +1,32 @@
 extern crate byteorder;
 
-use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+use std::net::{SocketAddr, Ipv4Addr, SocketAddrV4};
 use std::error::Error as StdError;
 
 use byteorder::{NetworkEndian, ByteOrder};
 
+fn spread_values_over_buffer(dest: &mut[u8], src: &[u8], start: usize) {
+    for (i, value) in src.iter().enumerate() {
+        dest[i + start] = *value;
+    }
+}
+
 #[derive(Debug)]
 pub struct Client {
-    pub private: SocketAddr,
-    pub public: SocketAddr,
-    pub target: IpAddr,
+    pub private: SocketAddrV4,
+    pub public: SocketAddrV4,
+    pub target: Ipv4Addr,
 }
 
 impl Client {
-    pub fn new(msg: &[u8], public: SocketAddr) -> Result<Client, Box<StdError>> {
+    pub fn new(msg: &[u8], public: SocketAddr) -> Result<Self, Box<StdError>> {
         let private = Client::parse_private_connection(msg);
         let target = Client::parse_target_ip(&msg[6..]);
+
+        let public = match public {
+            SocketAddr::V4(addr) => addr,
+            _ => panic!("Woah there that's not IPv4"),
+        };
 
         Ok(Client{
             private,
@@ -23,17 +34,34 @@ impl Client {
             target,
         })
     }
+
+    pub fn parse_private_connection(msg: &[u8]) -> SocketAddrV4 {
+        let ip = Ipv4Addr::new(msg[0], msg[1], msg[2], msg[3]);
+        let port = NetworkEndian::read_u16(&msg[4..6]);
+        SocketAddrV4::new(ip, port)
+    }
+
+    fn parse_target_ip(msg: &[u8]) -> Ipv4Addr {
+        Ipv4Addr::new(msg[0], msg[1], msg[2], msg[3])
+    }
 }
 
 impl Client {
-    fn parse_private_connection(msg: &[u8]) -> SocketAddr {
-        let ip = Ipv4Addr::new(msg[0], msg[1], msg[2], msg[3]);
-        let port = NetworkEndian::read_u16(&msg[4..6]);
-        SocketAddr::new(IpAddr::V4(ip), port)
-    }
-
-    fn parse_target_ip(msg: &[u8]) -> IpAddr {
-        IpAddr::V4(Ipv4Addr::new(msg[0], msg[1], msg[2], msg[3]))
+    // Serializes a Client struct into a array with the following values:
+    // Target Private IP: 4 bytes
+    // Target Private Port: 2 Bytes
+    // Target Public IP: 4 bytes
+    // Target Public Port: 2 Bytes
+    pub fn serialize(&self) -> [u8;12] {
+        let mut msg = [0; 12];
+        for (i, value) in self.private.ip().octets().iter().enumerate() {
+            msg[i] = *value;
+        }
+        spread_values_over_buffer(&mut msg, &self.private.ip().octets(), 0);
+        NetworkEndian::write_u16(&mut msg[4..6], self.private.port());
+        spread_values_over_buffer(&mut msg, &self.public.ip().octets(), 6);
+        NetworkEndian::write_u16(&mut msg[10..], self.public.port());
+        msg
     }
 }
 
@@ -50,16 +78,16 @@ impl Pool {
 }
 
 impl Pool {
-    pub fn client_in_pool(&self, target_ip: &IpAddr) -> Option<usize> {
+    pub fn client_in_pool(&self, target_ip: &Ipv4Addr) -> Option<usize> {
         for (i, ref client) in self.connections.iter().enumerate() {
-            if client.private.ip() == *target_ip {
+            if client.private.ip() == target_ip {
                 return Some(i);
             }
         }
         None
     }
 
-    pub fn find_client(&mut self, target_ip: &IpAddr) -> Option<Client> {
+    pub fn find_client(&mut self, target_ip: &Ipv4Addr) -> Option<Client> {
         match self.client_in_pool(&target_ip) {
             Some(pos) => return Some(self.connections.swap_remove(pos)),
             _ => return None,
